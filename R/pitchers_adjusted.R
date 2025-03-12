17bc1aaf1f910333f7a1f574e89310cacf313317
TUTU <zheerw2@illinois.edu>
2025-03-11 20:25:29 -0500

    Rename season_adjusted_2024 R scripts to adjusted

--- R/pitchers_adjusted.R
+++ R/pitchers_adjusted.R
@@ -1,7 +1,6 @@
-#' Era-adjusted Pitchers Dataset
+#' Era-adjusted Pitchers Career Dataset (2024)
 #'
-#' A dataset containing era-adjusted statistics for pitchers.
-#'
+#' A dataset containing era-adjusted season statistics for pitchers up to 2024.
 #' @details
 #' These era-adjusted statistics are obtained from Full House Modeling.
 #' This model computes era-adjusted statistics through a principled
@@ -13,22 +12,21 @@
 #' extends across eras.
 #'
 #' @usage data(pitchers_adjusted)
-#' @format A data frame with 30932 rows and 10 variables:
+#' @format A data frame with 31502 rows and 10 variables:
 #' \itemize{
-#'   \item name:  a character vector containing the name of the player.
 #'   \item playerID: The unique ID of the player.
+#'   \item name:  a character vector containing the name of the player.
 #'   \item age: The age of the player during the season (e.g., integer).
 #'   \item year: The season year (e.g., integer).
 #'   \item IP: Innings pitched, the total number of innings the pitcher has thrown (e.g., numeric).
+#'   \item ER:Earned Runs, the number of runs that scored against a pitcher that were not a result of fielding errors. 
 #'   \item ERA: Earned Run Average, calculated as (9 * ER)/IP (e.g., numeric).
-#'   \item SO: Strikeouts, the number of batters struck out by the pitcher (e.g., integer).
-#'   \item ebWAR: Era-adjusted Wins Above Replacement as computed by Baseball Reference (e.g., numeric).
-#'   \item efWAR: Era-adjusted Wins Above Replacement as computed by FanGraphs (e.g., numeric).
-#'   \item ER: Earned Runs, the number of runs scored off the pitcher that are not the result of errors or passed balls (e.g., integer).
+#'   \item K:strikeout, the number of times a pitcher struck out a batter. 
+#'   \item ebWAR: career era-adjusted wins above replacement as computed by Baseball Reference.
+#'   \item efWAR: career era-adjusted wins above replacement as computed by FanGraphs.
 #' }
 #' @references
 #' Shen Yan, Adrian Burgos Jr., Christopher Kinson, and Daniel J. Eck (2024). "Comparing baseball players across eras via novel Full House Modeling." Available at: \url{https://arxiv.org/abs/2207.11332}
 #'
 #' Website: \url{https://eckeraadjustment.web.illinois.edu/}
-#'
-"pitchers_adjusted"
+"pitchers_season_adjusted_2024"
